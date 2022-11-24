b94a9fee89f85db9ba0fc65fb0b8b0110f503acf
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-24 23:29:35 +0300

    Save comp style complited

--- src/components/SaveTask/SaveTask.js
+++ src/components/SaveTask/SaveTask.js
@@ -1,11 +1,16 @@
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 import React from 'react';
 import styles from './SaveTask.Style';
 
 const SaveTask = () => {
   return (
     <View style={styles.container}>
-      <Text>SaveTask</Text>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Görev Giriniz..." />
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.title}>Kaydet</Text>
+      </TouchableOpacity>
     </View>
   );
 };

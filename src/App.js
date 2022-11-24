7a14d6a8cd6a5b148880e51a797a7cca72d01b90
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-24 11:10:59 +0300

    Banner Complited

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import {View, SafeAreaView, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  const [number, setNumber] = useState(0);
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.bannerContainer}>
+        <Text style={styles.bannerText}> Yapılacaklar </Text>
+        <Text style={styles.bannerNumber}>{number}</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#112027',
+  },
+  bannerContainer: {
+    backgroundColor: 'orange',
+    flexDirection: 'row',
+    padding: 6,
+  },
+  bannerText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    flex: 1,
+  },
+  bannerNumber: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'right',
+    marginRight: 5,
+  },
+});
+export default App;

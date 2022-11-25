ee3e8f110c550a1c12a469467556f788384a912c
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-25 13:32:53 +0300

    Revert "Save comp style complited"
    This reverts commit b94a9fee89f85db9ba0fc65fb0b8b0110f503acf.

--- src/components/SaveTask/SaveTask.Style.js
+++ src/components/SaveTask/SaveTask.Style.js
@@ -2,25 +2,7 @@ import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    margin: 15,
-    padding: 10,
-    backgroundColor: 'lightgray',
-    borderRadius: 10,
+    backgroundColor: 'yellow',
   },
-  inputContainer: {
-    borderBottomWidth: 2,
-    borderColor: 'black',
-    padding: 5,
-  },
-  button: {
-    backgroundColor: 'gray',
-    alignItems: 'center',
-    margin: 10,
-    padding: 10,
-    borderRadius: 10,
-  },
-  title: {
-    color: 'white',
-    fontSize: 20,
-  },
+  text: {},
 });
--- src/components/SaveTask/SaveTask.js
+++ src/components/SaveTask/SaveTask.js
@@ -1,16 +1,11 @@
-import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
 import styles from './SaveTask.Style';
 
 const SaveTask = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput placeholder="Görev Giriniz..." />
-      </View>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.title}>Kaydet</Text>
-      </TouchableOpacity>
+      <Text>SaveTask</Text>
     </View>
   );
 };

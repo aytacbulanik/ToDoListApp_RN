bdcc66f86cde90d7abf9df49f3447b65a1e488bb
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-25 14:34:20 +0300

    mevcud yapı custom komponentler çıkarılarak kullanılmıştır.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import React, {useState} from 'react';
-import {View, SafeAreaView, Text, StyleSheet, FlatList} from 'react-native';
-import TaskLabel from './components/TaskLabel/TaskLabel';
-import SaveTask from './components/SaveTask/SaveTask';
+import {
+  View,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 const App = () => {
   const [number, setNumber] = useState(0);
   const [tasks, setTasks] = useState([]);
+  const [text, setText] = useState('');
 
-  const renderTask = item => {
-    <TaskLabel task={item} />;
-  };
+  const renderTaskItem = ({item}) => <Text style={styles.label}>{item}</Text>;
+
+  function listeyiGuncelle() {
+    setNumber(number + 1);
+    setTasks([...tasks, text]);
+  }
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.bannerContainer}>
         <Text style={styles.bannerText}> Yapılacaklar </Text>
         <Text style={styles.bannerNumber}>{number}</Text>
       </View>
-      <FlatList data={tasks} renderItem={renderTask} />
-      <SaveTask />
+      <FlatList
+        key={item => item.id}
+        data={tasks}
+        renderItem={renderTaskItem}
+      />
+      <View style={styles.savecontainer}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Görev Giriniz..."
+            onChangeText={setText}
+            value={text}
+          />
+        </View>
+        <TouchableOpacity style={styles.button} onPress={listeyiGuncelle}>
+          <Text style={styles.title}>Kaydet</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
@@ -44,6 +69,35 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'right',
     marginRight: 5,
+  },
+  savecontainer: {
+    margin: 15,
+    padding: 10,
+    backgroundColor: 'lightgray',
+    borderRadius: 10,
+  },
+  inputContainer: {
+    borderBottomWidth: 2,
+    borderColor: 'black',
+    padding: 5,
+  },
+  button: {
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+  },
+  title: {
+    color: 'white',
+    fontSize: 20,
+  },
+  label: {
+    backgroundColor: 'aqua',
+    margin: 6,
+    padding: 5,
+    fontSize: 16,
+    borderRadius: 6,
   },
 });
 export default App;

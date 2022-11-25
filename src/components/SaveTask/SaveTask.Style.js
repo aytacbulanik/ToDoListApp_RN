a118ec8c7658524f96b7a93bc0c3df479edbba8f
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-25 13:33:21 +0300

    Revert "Revert "Save comp style complited""
    This reverts commit ee3e8f110c550a1c12a469467556f788384a912c.

--- src/components/SaveTask/SaveTask.Style.js
+++ src/components/SaveTask/SaveTask.Style.js
@@ -2,7 +2,25 @@ import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    backgroundColor: 'yellow',
+    margin: 15,
+    padding: 10,
+    backgroundColor: 'lightgray',
+    borderRadius: 10,
   },
-  text: {},
+  inputContainer: {
+    borderBottomWidth: 2,
+    borderColor: 'black',
+    padding: 5,
+  },
+  button: {
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+  },
+  title: {
+    color: 'white',
+    fontSize: 20,
+  },
 });

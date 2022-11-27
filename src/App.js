820a1ea0e7ab3e6c3525707e9f7c1fc9bb734933
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-27 22:50:23 +0300

    top comp added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import {SafeAreaView, Text, View} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Deneme</Text>
+      <View style={styles.addContainer}>
+        <TextInput style={styles.addContainer} />
+        <TouchableOpacity>
+          <Text> Add </Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  addContainer: {flexDirection: 'row', backgroundColor: 'red'},
+  textInputContainer: {
+    margin: 10,
+    padding: 5,
+    height: 30,
+    borderRadius: 6,
+  },
+  button: {},
+});
 export default App;

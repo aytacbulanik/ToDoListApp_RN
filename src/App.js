6ea2661acbf88c20167bfedffa7c4b3319760e90
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-29 16:01:49 +0300

    task number added

--- src/App.js
+++ src/App.js
@@ -6,16 +6,31 @@ import {
   StyleSheet,
   TouchableOpacity,
   FlatList,
+  Alert,
 } from 'react-native';
 import React, {useState} from 'react';
 
 const App = () => {
   const [text, setText] = useState('');
+  const [taskNumber, setTaskNumber] = useState(0);
   const [textArray, setTextArray] = useState([]);
   function saveText() {
-    setTextArray([...textArray, text]);
-    console.log(textArray);
-    setText('');
+    if (text === '') {
+      Alert.alert('Lütfen bir görev giriniz');
+    } else {
+      setTextArray([...textArray, text]);
+      setTaskNumber(textArray.length + 1);
+      setText('');
+    }
+  }
+  function saveUpperText() {
+    if (text === '') {
+      Alert.alert('Lütfen bir görev giriniz');
+    } else {
+      setTextArray([...textArray, text.toUpperCase()]);
+      setText('');
+      setTaskNumber(textArray.length + 1);
+    }
   }
   const textRenderItem = ({item}) => <Text style={styles.Label}>{item}</Text>;
   return (
@@ -23,11 +38,22 @@ const App = () => {
       <Text style={styles.banner}> TO-DO LIST APP</Text>
       <View style={styles.container}>
         <View style={styles.addContainer}>
-          <TextInput value={text} onChangeText={setText} />
+          <TextInput
+            value={text}
+            onChangeText={setText}
+            placeholder="Bir görev yazınız"
+          />
         </View>
-        <TouchableOpacity style={styles.button} onPress={saveText}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={saveText}
+          onLongPress={saveUpperText}>
           <Text style={styles.buttonText}> ADD </Text>
         </TouchableOpacity>
+      </View>
+      <View style={styles.statusContainer}>
+        <Text style={styles.statusText}>Yapılacaklar</Text>
+        <Text style={styles.statusNumber}>{taskNumber}</Text>
       </View>
       <View>
         <FlatList data={textArray} renderItem={textRenderItem} />
@@ -76,6 +102,23 @@ const styles = StyleSheet.create({
     fontSize: 20,
     margin: 6,
     padding: 6,
+  },
+  statusContainer: {
+    flexDirection: 'row',
+    backgroundColor: 'aqua',
+    margin: 6,
+    padding: 6,
+    borderRadius: 8,
+  },
+  statusText: {
+    flex: 1,
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  statusNumber: {
+    marginRight: 10,
+    fontSize: 25,
+    fontWeight: 'bold',
   },
 });
 export default App;
